491cf707f941d466e20c5dd313134235ec1cfa35
edwardjuly <julyemail1@gmail.com>
2020-01-16 15:12:40 +0100

    first upload

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', ready);
+function ready() {
+  // wifiDetect();
+}
+
+function wifiDetect() {
+  let content = document.getElementById('wrapper');
+  let message = document.getElementById('intro');
+  handleStateChange();
+
+  function handleStateChange() {
+    if (navigator.onLine) {
+      content.classList.add('invisible');
+      message.classList.remove('invisible');
+    } else {
+      content.classList.remove('invisible');
+      message.classList.add('invisible');
+    }
+  }
+
+  window.addEventListener('online', handleStateChange);
+  window.addEventListener('offline', handleStateChange);
+}
